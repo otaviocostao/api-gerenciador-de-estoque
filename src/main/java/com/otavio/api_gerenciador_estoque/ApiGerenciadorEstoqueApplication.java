69c7a781ebf33980ee00968faa71ba2359d8e7d3
Otavio <dev.otaviocostao@gmail.com>
2025-02-13 11:02:01 -0300

    Arquivos base do Spring

---
+++ src/main/java/com/otavio/api_gerenciador_estoque/ApiGerenciadorEstoqueApplication.java
@@ -0,0 +1,13 @@
+package com.otavio.api_gerenciador_estoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGerenciadorEstoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGerenciadorEstoqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/otavio/api_gerenciador_estoque/ApiGerenciadorEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otavio.api_gerenciador_estoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGerenciadorEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
